2a459afbb1d319e088853e3d5cf125b40b112d86
EstebanGC <johan.gal.car@gmail.com>
2023-06-12 23:28:19 -0500

    Rendering the list of products and creating the adding product form

--- ecommerce/src/pages/ProductFormPage.jsx
+++ ecommerce/src/pages/ProductFormPage.jsx
@@ -1,7 +1,30 @@
+import {useForm } from 'react-hook-form'
+
 export function ProductFormPage() {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data)
+    })
+
     return(
         <div>
-            <h1>Produc Form Page</h1>
+            <form action="">
+                <input type="text" placeholder="Name" 
+                {...register("title", { required: true })}/>
+                <textarea rows="3" placeholder="Description"
+                {...register("description", { required: true })}></textarea>
+                <input type='number' placeholder='Price' 
+                {...register("price", {required: true})}/>
+                <textarea rows="1" placeholder="Quantity"
+                {...register("quantity", {required:true})}></textarea>
+                <input type="number" placeholder='Stock'
+                {...register("stock", {required:true})}/>
+                <input type="number" placeholder='Minimum units'
+                {...register("minUnits", {required:true})}/>
+                <button>Save</button>
+            </form>
         </div>
     )
 }
